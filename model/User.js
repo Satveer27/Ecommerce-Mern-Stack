366a0d69d3da11103fc18449b4c68fef05284be9
2785448g <2785448g@student.gla.ac.uk>
2023-12-22 07:56:36 +0800

    Completed issue#3
    This completes the creation of the user schema and ensures its a model.

---
+++ model/User.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+//Schema is the blueprint for the model
+const UserSchema = new Schema({
+    username:{
+        type:String, 
+        required: true,
+    },
+    email:{
+        type:String, 
+        required: true,
+    },
+    password:{
+        type:String, 
+        required: true,
+    },
+    orders:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Order",
+        }
+    ],
+    wishList:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "WishList",
+        }
+    ],
+    isAdmin:{
+        type:Boolean, 
+        default: false,
+    },
+    shippingAddress:{
+        firstName:{
+            type:String
+        },
+        lastName:{
+            type:String
+        },
+        address:{
+            type:String
+        },
+        city:{
+            type:String
+        },
+        postalCode:{
+            type:String
+        },
+        country:{
+            type:String
+        },
+        phone:{
+            type:String
+        },
+    },
+    },{
+    timestamps: true
+    //Date user is created and updated
+    }
+);
+
+//Compile schema to model
+const User = mongoose.model('User', UserSchema);
+
+export default User;

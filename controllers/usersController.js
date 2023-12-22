56e7601e6af39c8c63bf6b645f6627012d7a56c8
2785448g <2785448g@student.gla.ac.uk>
2023-12-22 09:34:39 +0800

    Completed hashing the password closes #6
    I completed the functionality to hash the passwords as well as made sure
    that users can only create accounts with a unique email instead of a
    duplicate email. I also fixed a error where users wont have trouble
    creating a account with a same username but different email.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 //Controller are where all business logic are stored
-import User from "../model/User.js"
+import User from "../model/User.js";
+import bcrypt from "bcryptjs";
 
 // @description Register users
 // @route Post /api/v1/users/register
@@ -9,23 +10,27 @@ import User from "../model/User.js"
 export const registerUserController = async(req, res)=>{
     const{username, email, password} = req.body;
     //Check if user exist
-    const userExists = await User.findOne({ email });
+    const userExists = await User.findOne({ email});
     if(userExists){
         res.json({
             msg: 'User alrdy exist'
         })
     }
-    //Hash password
-
-    //create user
-    const user = await User.create({
-        username, 
-        email, 
-        password,
-    });
-    res.status(201).json({
-        status: 'success',
-        msg: 'User registered succesfully',
-        data: user,
-    })
+    else{
+        //Hash password
+        const salt = await bcrypt.genSalt(15);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        //create user
+        const user = await User.create({
+            username, 
+            email, 
+            password: hashedPassword,
+        });
+        res.status(201).json({
+            status: 'success',
+            msg: 'User registered succesfully',
+            data: user,
+         })
+    }
+    
 };

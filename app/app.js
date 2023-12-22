fe28ea6a3bb3a686454d98ff55110b2f984955ec
2785448g <2785448g@student.gla.ac.uk>
2023-12-22 08:23:20 +0800

    Completed connecting to the database which closes issue#4
    Completed connecting to the database as well as creating env files to
    securely ensure password is not exposed

--- app/app.js
+++ app/app.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import dbConnect from '../config/dbConnect.js';
+import dotenv from 'dotenv';
+
+//Have access to variable in env file
+dotenv.config();
+
+//Database connection
+dbConnect();
 
 const app = express();
 

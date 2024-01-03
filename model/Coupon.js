2c12f9cda8316d97a2ac40e43952b2cd388b1528
2785448g <2785448g@student.gla.ac.uk>
2024-01-03 19:35:21 +0800

    Completed validating the coupon dates closes #40
    Completed validating coupon expiry date, creation dates and end dates.

--- model/Coupon.js
+++ model/Coupon.js
@@ -24,7 +24,46 @@ const CouponSchema = new Schema({
         ref:"User",
         required:true,
     }
-},{timestamps:true})
+},{timestamps:true, toJSON:{virtuals:true},})
+
+//coupon is expired
+CouponSchema.virtual('isExpired').get(function(){
+    return this.endDate< Date.now();
+});
+
+//validation
+//check if enddate<startdate
+CouponSchema.pre('validate', function(next){
+    if(this.endDate<this.startDate){
+        next(new Error('End date cant be less than start date'));
+    }
+    next();
+});
+
+//check discount is less than 0
+CouponSchema.pre('validate', function(next){ 
+    if(this.discount <= 0 || this.discount>100){
+        next(new Error('Discount cant be less than 0 or more than 100'));
+    }
+    next();
+});
+
+//starting date cant be less than current date
+CouponSchema.pre('validate', function(next){ 
+    if(this.startDate< Date.now()){
+        next(new Error('Start date cant be less than today'));
+    }
+    next();
+});
+
+//end date cant be less than current date
+CouponSchema.pre('validate', function(next){ 
+    if(this.endDate< Date.now()){
+        next(new Error('End date cant be less than today'));
+    }
+    next();
+});
+
 
 const Coupon = mongoose.model("Coupon", CouponSchema);
 export default Coupon;
